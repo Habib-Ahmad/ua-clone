7529f86c7926b8af5d044075039637453f18704e
Ahmad Habib <ahmadalkali69@gmail.com>
2022-11-15 10:46:00 +0100

    Setup colors

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: "#246BFD",
+  primaryDark: "#2F6FE3",
+  bg: "#ffffff",
+  text: "#000000",
+  textLight: "#00000080",
+  white: "#ffffff",
+  black: "#000000",
+  blackTransparent: "#e9f0ff26",
+};
