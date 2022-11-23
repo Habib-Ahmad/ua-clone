5dd33eaae4a7c6e5923727b48dd0793c472f5ba0
Ahmad Habib <ahmadalkali69@gmail.com>
2022-11-23 11:45:53 +0100

    Created Money Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,24 +1,53 @@
+import { useEffect, useState } from "react";
 import {
+  Animated,
   Dimensions,
   Image,
+  LayoutAnimation,
+  Platform,
   StatusBar,
   StyleSheet,
   Text,
   TouchableOpacity,
+  UIManager,
   View,
 } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { useIsFocused } from "@react-navigation/native";
-import LeftArrow from "../assets/LeftArrow";
+import BackArrowDark from "../assets/BackArrowDark";
 import Notification from "../assets/Notification";
-import RightArrow from "../assets/RightArrow";
 import Share from "../assets/Share";
+import Topup from "../assets/Topup";
+import Transfer from "../assets/Transfer";
 import Wave from "../assets/Wave";
+import Withdraw from "../assets/Withdraw";
 import { colors } from "../utils/colors";
 import CryptoScreen from "./CryptoScreen";
 import MoneyScreen from "./MoneyScreen";
 
+if (Platform.OS === "android" && UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
 const HomeScreen = () => {
+  const [active, setActive] = useState();
+  const [expanded, setExpanded] = useState(false);
+
+  useEffect(() => {
+    if (!active) {
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+      setExpanded(false);
+    } else {
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+      setExpanded(true);
+    }
+  }, [active]);
+
+  const total = moneyWalllets.reduce(
+    (accumulator, currentValue) => accumulator + Number(currentValue.balance),
+    0
+  );
+
   const isFocused = useIsFocused();
   const Tab = createMaterialTopTabNavigator();
 
@@ -26,40 +55,55 @@ const HomeScreen = () => {
     <View style={styles.container}>
       {isFocused && <StatusBar barStyle="light-content" backgroundColor={colors.primary} />}
 
-      <View style={styles.waveWrapper}>
-        <View style={styles.header}>
-          <View style={styles.headerTop}>
+      <View style={styles.header}>
+        <View style={styles.headerTop}>
+          {active ? (
+            <TouchableOpacity activeOpacity={0.6} onPress={() => setActive()}>
+              <BackArrowDark />
+            </TouchableOpacity>
+          ) : (
             <Image style={styles.logo} source={require("../assets/logo-white.png")} />
+          )}
 
-            <View style={styles.icons}>
-              <TouchableOpacity activeOpacity={0.6} onPress={() => {}}>
-                <Share />
-              </TouchableOpacity>
-              <View style={styles.space} />
-              <TouchableOpacity activeOpacity={0.6} onPress={() => {}}>
-                <Notification />
-              </TouchableOpacity>
-            </View>
-          </View>
-
-          <View style={styles.headerBottom}>
+          <View style={styles.icons}>
             <TouchableOpacity activeOpacity={0.6} onPress={() => {}}>
-              <LeftArrow />
+              <Share />
             </TouchableOpacity>
-
-            <View>
-              <Text style={styles.balanace}>₦554,200,778</Text>
-              <Text style={styles.balanaceText}>Balanace Available</Text>
-            </View>
-
+            <View style={styles.space} />
             <TouchableOpacity activeOpacity={0.6} onPress={() => {}}>
-              <RightArrow />
+              <Notification />
             </TouchableOpacity>
           </View>
         </View>
-        <View style={styles.wave}>
-          <Wave />
+
+        <View style={styles.headerMiddle}>
+          <View>
+            <Text style={styles.balance}>
+              {active || `₦‎${String(total).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}
+            </Text>
+            <Text style={styles.balanceText}>Balance Available</Text>
+          </View>
         </View>
+
+        {expanded && (
+          <Animated.View style={styles.headerBottom}>
+            <TouchableOpacity activeOpacity={0.6} onPress={() => {}}>
+              <Topup />
+              <Text style={styles.actionText}>Topup</Text>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.6} onPress={() => {}}>
+              <Transfer />
+              <Text style={styles.actionText}>Transfer</Text>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.6} onPress={() => {}}>
+              <Withdraw />
+              <Text style={styles.actionText}>Withdraw</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        )}
+      </View>
+      <View style={styles.wave}>
+        <Wave />
       </View>
 
       <Tab.Navigator
@@ -72,7 +116,9 @@ const HomeScreen = () => {
           tabBarPressColor: colors.bg,
         }}
       >
-        <Tab.Screen name="Money" component={MoneyScreen} />
+        <Tab.Screen name="Money">
+          {() => <MoneyScreen wallets={moneyWalllets} setActive={setActive} />}
+        </Tab.Screen>
         <Tab.Screen name="Crypto" component={CryptoScreen} />
       </Tab.Navigator>
     </View>
@@ -85,20 +131,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  waveWrapper: {
-    paddingBottom: 100,
-    position: "relative",
-    minHeight: Dimensions.get("screen").height / 3,
-  },
   header: {
     backgroundColor: colors.primary,
     paddingHorizontal: 20,
-    paddingBottom: 10,
-    height: 200,
   },
   wave: {
-    position: "absolute",
-    bottom: 10,
+    bottom: 5,
     left: 0,
     right: 0,
   },
@@ -107,6 +145,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     marginBottom: 40,
+    height: 80,
   },
   logo: {
     maxWidth: "30%",
@@ -120,19 +159,30 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
   },
+  headerMiddle: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   headerBottom: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+    paddingHorizontal: "10%",
+    paddingTop: 30,
   },
-  balanace: {
+  actionText: {
+    color: colors.white,
+    marginTop: 5,
+  },
+  balance: {
     color: colors.white,
     fontSize: 26,
     fontWeight: "700",
     textAlign: "center",
     marginBottom: 5,
   },
-  balanaceText: {
+  balanceText: {
     color: colors.white,
     fontWeight: "200",
     textAlign: "center",
@@ -159,3 +209,36 @@ const styles = StyleSheet.create({
     marginHorizontal: Dimensions.get("screen").width / 8,
   },
 });
+
+const moneyWalllets = [
+  {
+    wallet: "Naira (NGN)",
+    symbol: "₦‎",
+    balance: "5221008",
+    percentage: "2.2%",
+  },
+  {
+    wallet: "Pounds (GBP)",
+    symbol: "£",
+    balance: "5221008",
+    percentage: "2.2%",
+  },
+  {
+    wallet: "Canadian Dollars (CAD)",
+    symbol: "$",
+    balance: "5221008",
+    percentage: "2.2%",
+  },
+  {
+    wallet: "Euros (EUR)",
+    symbol: "€",
+    balance: "5221008",
+    percentage: "2.2%",
+  },
+  {
+    wallet: "Dollars (USD)",
+    symbol: "$",
+    balance: "5221008",
+    percentage: "2.2%",
+  },
+];
--- src/screens/MoneyScreen.js
+++ src/screens/MoneyScreen.js
@@ -1,9 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { colors } from "../utils/colors";
 
-const MoneyScreen = () => {
+const MoneyScreen = ({ wallets, setActive }) => {
   return (
     <View style={styles.container}>
-      <Text>MoneyScreen</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {wallets?.map((item) => (
+          <TouchableOpacity
+            key={item.wallet}
+            style={styles.wallet}
+            onPress={() =>
+              setActive(`${item.symbol}${item.balance.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`)
+            }
+          >
+            <Text style={styles.symbol}>{item.symbol}</Text>
+
+            <Text style={styles.walletName}>{item.wallet}</Text>
+
+            <View style={styles.balanceWrapper}>
+              <Text style={styles.balance}>{`${item.symbol}${item.balance.replace(
+                /\B(?=(\d{3})+(?!\d))/g,
+                ","
+              )}`}</Text>
+              <Text style={styles.percentage}>{item.percentage}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 };
@@ -11,5 +34,42 @@ const MoneyScreen = () => {
 export default MoneyScreen;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    paddingHorizontal: 10,
+  },
+  wallet: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  symbol: {
+    backgroundColor: colors.primaryLight,
+    color: colors.primary,
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontSize: 30,
+  },
+  walletName: {
+    flex: 1,
+    paddingLeft: 15,
+    fontSize: 16,
+  },
+  balance: {
+    textAlign: "right",
+    fontWeight: "600",
+    color: colors.primary,
+  },
+  percentage: {
+    textAlign: "right",
+    color: colors.green,
+    padding: 2,
+    backgroundColor: colors.greenLight,
+    borderRadius: 12,
+    width: 40,
+    marginLeft: "auto",
+  },
 });
