c56a4991fabeed1d8de0ff86cfd0a604d69f6b29
Ahmad Habib <ahmadalkali69@gmail.com>
2022-11-25 10:33:38 +0100

    Rescructured the navigation

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -1,51 +1,15 @@
-import { StyleSheet } from "react-native";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import History from "../assets/History";
-import Home from "../assets/Home";
-import Profile from "../assets/Profile";
-import { colors } from "../utils/colors";
-import HistoryStack from "./HistoryStack";
-import HomeStack from "./HomeStack";
-import ProfileStack from "./ProfileStack";
+import { createStackNavigator } from "@react-navigation/stack";
+import TopupScreen from "../screens/TopupScreen";
+import BottomTabs from "./BottomTabs";
 
-const BottomTab = createMaterialBottomTabNavigator();
-
-const styles = StyleSheet.create({
-  bar: {
-    backgroundColor: colors.bg,
-    justifyContent: "center",
-  },
-});
+const MainStack = createStackNavigator();
 
 const MainStackScreen = () => {
   return (
-    <BottomTab.Navigator activeColor={colors.primary} barStyle={styles.bar}>
-      <BottomTab.Screen
-        name="HomeStackScreen"
-        component={HomeStack}
-        options={{
-          tabBarIcon: ({ color }) => <Home color={color} />,
-          tabBarLabel: "Home",
-        }}
-      />
-      <BottomTab.Screen
-        name="History"
-        component={HistoryStack}
-        options={{
-          tabBarIcon: ({ color }) => <History color={color} />,
-          tabBarLabel: "History",
-          tabBarLabelStyle: { color: "green" },
-        }}
-      />
-      <BottomTab.Screen
-        name="Profile"
-        component={ProfileStack}
-        options={{
-          tabBarIcon: ({ color }) => <Profile color={color} />,
-          tabBarLabel: "Profile",
-        }}
-      />
-    </BottomTab.Navigator>
+    <MainStack.Navigator screenOptions={{ headerShown: false }}>
+      <MainStack.Screen name="Home" component={BottomTabs} />
+      <MainStack.Screen name="TopupScreen" component={TopupScreen} />
+    </MainStack.Navigator>
   );
 };
 
